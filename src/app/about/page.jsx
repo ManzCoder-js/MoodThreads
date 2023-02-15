f5fd9ba804399b2990dcf5aeeab72e2914e332df
hectormanza <mahector420@gmail.com>
2023-02-14 22:46:26 -0600

    nuevos archivos: src/app/Posts/page.jsx 	nuevos archivos: src/app/about/page.jsx 	nuevos archivos: src/app/components/Navigation.module.css 	nuevos archivos: src/app/components/navigation.jsx 	modificados:     src/app/globals.css 	modificados:     src/app/head.js 	modificados:     src/app/layout.js 	borrados:        src/app/page.js 	nuevos archivos: src/app/page.jsx 	borrados:        src/app/page.module.css

---
+++ src/app/about/page.jsx
@@ -0,0 +1,5 @@
+export default function AboutPage () {
+    return (
+        <h1>esta es la pagina AboutPage</h1>
+    )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,5 @@
+export default function HomePage() {
+  return (
+    <h1>hola mundo este contenedor esta genial por que le cabe lo que yo quiera.</h1>
+    )
+}
